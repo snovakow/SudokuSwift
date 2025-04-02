ef2164799701f8ff791dbc5c728786f36a433551
snovakow <snovakow@gmail.com>
2025-04-02 15:22:25 -0400

    Project Template

---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Scott on 2025-04-02.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
